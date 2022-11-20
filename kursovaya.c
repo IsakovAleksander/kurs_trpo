738d12bbbb7d3753f0db47c769d08c8a6dd1d508
IsakovAleksander <a.isakov04@gmail.com>
2022-11-20 15:04:35 +0700

    feature_init

--- kursovaya.c
+++ kursovaya.c
@@ -8,7 +8,37 @@ typedef struct Tariff{
     int cost;
 }Tariff;
 
+int cheapest(Tariff* tariff, int size){
+    int count = 1;
+    int extraMinutes = 5;
+    int extraMessages = 5;
+    int extraInternet = 50;
+    int mincost = tariff[1].cost + tariff[2].cost + tariff[3].cost;
+    Tariff buff;
+    for(int i = 1; i < size; i++){
+        buff.minutes = tariff[0].minutes - tariff[i].minutes;
+        if(buff.minutes < 0){
+            buff.minutes = 0;
+        }
+        buff.messages = tariff[0].messages - tariff[i].messages;
+        if(buff.messages < 0){
+            buff.messages = 0;
+        }
+        buff.internet = tariff[0].internet - tariff[i].internet;
+        if(buff.internet < 0){
+            buff.internet = 0;
+        }
+        buff.cost = tariff[i].cost + buff.minutes * extraMinutes + buff.messages * extraMessages + buff.internet * extraInternet;
+        if(mincost > buff.cost){
+            mincost = buff.cost;
+            count = i;
+        }
+    }
+    return count;
+}
+
 int main(){
+    int count;
     Tariff tariff[4] = {
         {},
         {300, 300, 15, 300},
